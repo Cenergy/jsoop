7ef0fc1ea1271730e6c8440f3f7ca7088f9d518e
Cenergy <673598118@qq.com>
2019-06-26 23:38:28 +0800

    http

--- node/app.js
+++ node/app.js
@@ -1,1 +1,11 @@
 console.log("hello world");
+
+const http = require("http");
+
+const httpserver = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("hello world");
+    res.end();
+  }
+});
+httpserver.listen(3000);
